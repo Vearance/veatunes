d4b366f0317a50fb93afaba519320076693d0ca3
Nathaniel Christian <nathaniel.christian@gmail.com>
2025-11-01 07:41:20 +0700

    feat: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,29 @@ export const metadata: Metadata = {
   title: {
     template: `%s | veatunes`,
     default: "veatunes",
-  }
+  },
+  icons: {
+    icon: [
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        url: "/favicon/favicon-32x32.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        url: "/favicon/favicon-16x16.png",
+      },
+    ],
+    apple: {
+      rel: "apple-touch-icon",
+      url: "/favicon/apple-touch-icon.png",
+      sizes: "180x180",
+      type: "image/png",
+    },
+  },
 };
 
 export default function RootLayout({
