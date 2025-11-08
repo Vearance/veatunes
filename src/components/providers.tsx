15f2f389f233250e5a19a8e52828496e5bf2e829
Nathaniel Christian <nathaniel.christian@gmail.com>
2025-11-08 19:37:49 +0700

    feat: add player-context to providers

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,11 +1,14 @@
 import { NavidromeProvider } from "@/components/navidrome-context";
+import { PlayerProvider } from '@/components/player-context';
 import { PlayerBar } from "@/components/player-bar";
 
 export function Providers({ children }: { children: React.ReactNode }) {
     return (
         <NavidromeProvider>
-            {children}
-            <PlayerBar />
+            <PlayerProvider>
+                {children}
+                <PlayerBar />
+            </PlayerProvider>
         </NavidromeProvider>
     );
 }
