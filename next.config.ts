b210ae6032e74e9d69b3a800a4f7a14a1b933608
Nathaniel Christian <nathaniel.christian@gmail.com>
2026-02-15 07:44:31 +0700

    chore: fix nextjs image pattern

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,22 @@
 import type { NextConfig } from "next";
+import type { RemotePattern } from "next/dist/shared/lib/image-config";
+
+const navidromeUrl = process.env.NAVIDROME_URL;
+
+if (!navidromeUrl) {
+  throw new Error("NAVIDROME_URL is not defined");
+}
+
+const url = new URL(navidromeUrl);
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: '161.33.170.91',
-        port: '4533',
-        pathname: '/rest/**',
+        protocol: url.protocol.replace(":", "") as RemotePattern["protocol"],
+        hostname: url.hostname,
+        port: url.port,
+        pathname: "/rest/**",
       },
     ],
   },
