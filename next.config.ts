1132b5f6d417ba9879c0872aaeb3b2ef4219a3b3
Nathaniel Christian <nathaniel.christian@gmail.com>
2026-02-16 23:45:23 +0700

    chore: add remote pattern for image

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,28 @@ import type { NextConfig } from "next";
 import type { RemotePattern } from "next/dist/shared/lib/image-config";
 
 const navidromeUrl = process.env.NAVIDROME_URL;
+const navidromePublicUrl = process.env.NEXT_PUBLIC_NAVIDROME_URL;
 
 if (!navidromeUrl) {
   throw new Error("NAVIDROME_URL is not defined");
 }
+if (!navidromePublicUrl) {
+  throw new Error("NAVIDROME_PUBLIC_URL is not defined");
+}
 
 const url = new URL(navidromeUrl);
+const publicUrl = new URL(navidromePublicUrl);
 
 const nextConfig: NextConfig = {
   output: "standalone",
   images: {
     remotePatterns: [
+    {
+        protocol: publicUrl.protocol.replace(":", "") as RemotePattern["protocol"],
+        hostname: publicUrl.hostname,
+        port: publicUrl.port,
+        pathname: "/rest/**",
+      },
       {
         protocol: url.protocol.replace(":", "") as RemotePattern["protocol"],
         hostname: url.hostname,
