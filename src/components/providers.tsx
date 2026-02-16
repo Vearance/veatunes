8ca12b58c603b8fb38473490c8aca28d8a136c41
Nathaniel Christian <nathaniel.christian@gmail.com>
2026-02-16 13:44:06 +0700

    refactor: wrap ui provider

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,13 +1,16 @@
 import { NavidromeProvider } from "@/components/navidrome-context";
 import { PlayerProvider } from '@/components/player-context';
+import { UIProvider } from '@/components/ui-context';
 import { PlayerBar } from "@/components/player-bar";
 
 export function Providers({ children }: { children: React.ReactNode }) {
     return (
         <NavidromeProvider>
             <PlayerProvider>
-                {children}
-                <PlayerBar />
+                <UIProvider>
+                    {children}
+                    <PlayerBar />
+                </UIProvider>
             </PlayerProvider>
         </NavidromeProvider>
     );
