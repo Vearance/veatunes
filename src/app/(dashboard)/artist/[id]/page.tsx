488cd16aa1d095ae417927105527a57a2c6c0715
Nathaniel Christian <nathaniel.christian@gmail.com>
2026-03-30 06:50:01 +0700

    refactor: revert mobile title version

--- src/app/(dashboard)/artist/[id]/page.tsx
+++ src/app/(dashboard)/artist/[id]/page.tsx
@@ -178,7 +178,7 @@ export default function ArtistDetailPage() {
 
                 <div className="flex flex-col sm:justify-end sm:h-[170px] items-center sm:items-start text-center sm:text-left">
                     <div>
-                        <h1 className="text-5xl md:text-6xl font-extrabold text-zinc-100 tracking-wide">
+                        <h1 className="text-xl md:text-6xl font-semibold md:font-extrabold text-zinc-100 md:tracking-wide">
                             {artist.name}
                         </h1>
                         <p className="text-sm text-zinc-500 mt-2">
